be598db997c729e962a931e17ad5e4815aa84931
Albert Tang <tangalbert919@yahoo.com>
2023-05-11 13:08:45 -0500

    sm8350-common: Execute A/B post-install scripts
    Change-Id: If086cd5ce641dc1a084d55d261a17b3c42866ffd

--- common.mk
+++ common.mk
@@ -68,6 +68,23 @@ PRODUCT_COPY_FILES += \
 # Properties
 PRODUCT_COMPATIBLE_PROPERTY_OVERRIDE := true
 
+# A/B
+AB_OTA_POSTINSTALL_CONFIG += \
+    RUN_POSTINSTALL_system=true \
+    POSTINSTALL_PATH_system=system/bin/otapreopt_script \
+    FILESYSTEM_TYPE_system=ext4 \
+    POSTINSTALL_OPTIONAL_system=true
+
+AB_OTA_POSTINSTALL_CONFIG += \
+    RUN_POSTINSTALL_vendor=true \
+    POSTINSTALL_PATH_vendor=bin/checkpoint_gc \
+    FILESYSTEM_TYPE_vendor=ext4 \
+    POSTINSTALL_OPTIONAL_vendor=true
+
+PRODUCT_PACKAGES += \
+    checkpoint_gc \
+    otapreopt_script
+
 # ANT+
 PRODUCT_PACKAGES += \
     AntHalService
